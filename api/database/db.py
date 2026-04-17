c2f6ed4d52bd3f15daa3189362e78e2e964f95f9
Emediong Jonah <emediongjonah66@gmail.com>
2026-04-17 01:01:41 +0100

    db url add

--- api/database/db.py
+++ api/database/db.py
@@ -8,13 +8,10 @@ pool = None
 async def create_pool():
     global pool
     pool = await asyncpg.create_pool(
-        user=os.getenv("POSTGRES_USER"),
-        password=os.getenv("POSTGRES_PASSWORD"),
-        database=os.getenv("POSTGRES_DB"),
-        host=os.getenv("POSTGRES_HOST"),
-        port=int(os.getenv("POSTGRES_PORT")),
+        dsn=os.getenv("DATABASE_URL"),
         min_size=1,
         max_size=10,
+        ssl="require"
     )
 
 
