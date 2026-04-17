242060bdac2191c4f2e2fd2158c7e8a5ca9b1589
Emediong Jonah <emediongjonah66@gmail.com>
2026-04-17 07:51:25 +0100

    redirect slashes

--- api/index.py
+++ api/index.py
@@ -26,7 +26,7 @@ async def lifespan(app: FastAPI):
     app.state.pool = await close_pool()
     print("DB closed")
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=lifespan, redirect_slashes=False)
 handler = Mangum(app)
 
 app.add_middleware(
